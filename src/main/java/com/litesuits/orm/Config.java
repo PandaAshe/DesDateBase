78c9c0a9f649dc4481c2fe3f987a904745bdd436
Administrator <qq>
2017-10-25 09:51:54 +0800

    Initial commit

---
+++ src/main/java/com/litesuits/orm/Config.java
@@ -0,0 +1,11 @@
+package com.litesuits.orm;
+
+/**
+ * Created by 吴志华 on 2017/10/24.
+ *
+ * @FileName:com.litesuits.orm.Config.java
+ */
+
+public class Config {
+    public static String PW = "#######";
+}
